b77824e5a1d95bd0ea05e576f80f31484500e6e9
Alan Thatcher <alanwthatcher@gmail.com>
2019-02-05 18:26:48 -0500

    updates

--- wordpress/plan.sh
+++ wordpress/plan.sh
@@ -3,7 +3,7 @@
 
 pkg_name=wordpress
 pkg_origin=alan
-pkg_version="5.0.3"
+pkg_version="4.9.5"
 pkg_maintainer="The Habitat Maintainers <humans@habitat.sh>"
 pkg_license=('Apache-2.0')
 pkg_source="https://wordpress.org/${pkg_name}-${pkg_version}.tar.gz"
