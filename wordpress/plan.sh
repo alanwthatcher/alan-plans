77a6983774a586e0c08b42bebdea87385260e4c1
Alan Thatcher <alanwthatcher@gmail.com>
2019-02-05 19:48:38 -0500

    reset world

---
+++ wordpress/plan.sh
@@ -0,0 +1,35 @@
+# This file is the heart of your application's habitat.
+# See full docs at https://www.habitat.sh/docs/reference/plan-syntax/
+
+pkg_name=wordpress
+pkg_origin=azure-habitat-example
+pkg_version="5.0.3"
+pkg_maintainer="The Habitat Maintainers <humans@habitat.sh>"
+pkg_license=('Apache-2.0')
+pkg_source="https://wordpress.org/${pkg_name}-${pkg_version}.tar.gz"
+pkg_shasum="9aa4d1bc9acc39279f15e914415be87af01a886287b1b92b3a80426a4edbd78a"
+pkg_description="installs wordpress"
+pkg_upstream_url="https://wordpress.org/"
+
+source_dir=$HAB_CACHE_SRC_PATH/${pkg_name}
+
+pkg_svc_user=root
+pkg_svc_group=$pkg_svc_user
+
+pkg_deps=(core/php core/curl core/nginx core/mysql-client core/readline)
+
+pkg_exports=()
+pkg_exposes=()
+
+pkg_binds=(
+  [database]="host port username password database"
+)
+
+
+do_build(){
+  return 0
+}
+
+do_install() {
+  cp -r "$source_dir" "$pkg_prefix/wordpress/"
+}
